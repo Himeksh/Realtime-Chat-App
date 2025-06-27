1728978a04d64c866426ba2d89009d5efc06e914
Himeksh <himekshkhanna368@gmail.com>
2025-06-28 01:09:55 +0530

    work

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,13 +6,18 @@ import cookieParser from 'cookie-parser';
 import { connectDB } from "./lib/db.js";
 import cors from 'cors';
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -23,7 +28,13 @@ app.use(cors({
 }))
 
 app.use("/api/auth", authRoutes);
+console.log("Auth routes registered");
+
 app.use("/api/messages", messageRoutes);
+console.log("Message routes registered");
+
+console.log("Serving frontend static files from:", path.join(__dirname, "../frontend/dist"));
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
